924a1fa325a4629974e957a1f6472f347bca23f2
Lucas Mendes <luckasmendys72@hotmail.com>
2022-11-23 11:11:53 -0300

    adicionando a lógica para correta contagem até os minutos

--- main.js
+++ main.js
@@ -2,23 +2,25 @@ const botaoIniciar = document.getElementById('botao-iniciar');
 const botaoPausar = document.getElementById('botao-pausar');
 var cronometro = document.querySelector('.box');
 var segundos = 0;
+var minutos = 0;
+var horas = 0;
 
 botaoIniciar.addEventListener('click', () => {
+    // lembrar de alterar o tempo de execução da função para milisegundos
     contador = setInterval(mudaCronometro, 1000)
 })
 
 function mudaCronometro(){
     segundos++
-    cronometro.innerHTML = '00:'+'00:'+'0'+segundos
-    if(segundos > 9){
-        cronometro.innerHTML = '00:'+'00:'+segundos
+    cronometro.innerHTML = horas+':'+minutos+':'+segundos
+    //lembrar de consertar para > 59 segundos quando arrumar o contador
+    if(segundos > 58){
+        segundos = 0
+        minutos += 1
     }
-    if(segundos > 59){
-        cronometro.innerHTML = '00:'+'0'+'1'
-    }
-}
-
-
+    
+}    
+ 
 botaoPausar.addEventListener('click', () => {
     clearInterval(contador)
 })
